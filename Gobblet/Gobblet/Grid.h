ac02a92bc828660b6c2f13eaae96b0cc4712244d
Andrew Greenslade <andrew.greenslade71@yahoo.com>
2023-01-11 22:13:24 +0000

    added comments to grid class

--- Gobblet/Gobblet/Grid.h
+++ Gobblet/Gobblet/Grid.h
@@ -18,17 +18,19 @@ public:
 	sf::Vector2f offset{ 350, 0 };
 
 	Cell* at(int t_index);
+	
 	Grid();
 	~Grid();
+	
 	void setupGrid();
 	void draw(sf::RenderWindow& m_window);
-	void reset();
 	void update(sf::RenderWindow& t_window);
+
 	sf::Color defaultColor{ 150, 78, 0, 255 };
 	sf::Color highlightedColor{ 170, 98, 20, 255 };
 
-
 	sf::Font idFont;
+
 	Cell cellGrid[XAmountCells][YAmountCells];
 
 	Cell* GetIntersects(sf::Vector2f pos);
